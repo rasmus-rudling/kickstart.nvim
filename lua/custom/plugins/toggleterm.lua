4f59a1cce95a40c42d46bab7992d0378a615de58
Rasmus Rudling <rasmusrudling@gmail.com>
2024-09-07 14:34:23 +0200

    feat: Make it possible to open files from lazy git into current neovim runtime.

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -6,5 +6,19 @@ return {
       direction = 'float',
       open_mapping = [[<C-\>]],
     }
+
+    local Terminal = require('toggleterm.terminal').Terminal
+    local lazygit = Terminal:new { cmd = 'lazygit', hidden = true, close_on_exit = true }
+
+    function _LAZYGIT_TOGGLE()
+      lazygit:toggle()
+    end
+
+    vim.api.nvim_set_keymap('n', '<leader>g', '<cmd>lua _LAZYGIT_TOGGLE()<CR>', { noremap = true, silent = true })
+
+    -- To open files with <e> in lazygit that opens a new buffer inside the current neo-vim process:
+    -- /Users/rr/Library/Application Support/lazygit
+    -- os:
+    -- editPreset: 'nvim-remote'
   end,
 }
